35aef9f32e6b18e0671e75cafe8ac073e14da430
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-02-15 22:59:56 +0900

    0215

--- Day15/js.js
+++ Day15/js.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
 function addItem(e) {
   e.preventDefault();
@@ -13,17 +13,22 @@ function addItem(e) {
   };
   items.push(item);
   populateList(items, itemsList);
+  localStorage.setItem("items", JSON.parse(JSON.stringify(items)));
 }
 
 function populateList(plates = [], platesList) {
   platesList.innerHTML = plates
     .map((plate, i) => {
       return `
-        <li>
-            <label for="">${plate.text}</label> 
-        </li>
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${
+        plate.done ? "checked" : ""
+      } />
+        <label for="item${i}">${plate.text}</label>
+      </li>
       `;
     })
     .join("");
 }
 addItems.addEventListener("submit", addItem);
+populateList(items, itemsList);
