6337e407e32448c2ccf57255b78f9bbc3c4bb788
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-02-19 11:29:23 +0900

    Add highScore

--- Day30/doit.js
+++ Day30/doit.js
@@ -2,7 +2,11 @@ const holes = document.querySelectorAll(".hole");
 const moles = document.querySelectorAll(".mole");
 const scoreBoard = document.querySelector(".score");
 const startButton = document.querySelector(".gamestart-button");
+const highScoreBoard = document.querySelector(".highscore");
 let lastHole;
+let lastScore = 0;
+let currentScore = 0;
+let highScore = 0;
 let timeUp = false;
 let score = 0;
 
@@ -14,7 +18,7 @@ function randomHole(holes) {
   const idx = Math.floor(Math.random() * holes.length);
   const hole = holes[idx];
   if (hole === lastHole) {
-    console.log("재실행");
+    // console.log("재실행");
     return randomHole(holes);
   }
   lastHole = hole;
@@ -32,17 +36,33 @@ function peep() {
 }
 
 function startGame() {
+  score = 0;
   timeUp = false;
+  scoreBoard.textContent = score;
   peep();
-  setTimeout(() => (timeUp = true), 15000);
+  setTimeout(() => {
+    timeUp = true;
+    currentScore = localStorage.getItem("currentScore");
+    if (currentScore > highScore) {
+      localStorage.setItem("highScore", currentScore);
+      highScore = localStorage.getItem("highScore");
+      localStorage.setItem("currentScore", 0);
+      scoreBoard.textContent = "0";
+      highScoreBoard.textContent = highScore;
+    }
+  }, 7000);
 }
 
 function bonk(e) {
   if (!e.isTrusted) return;
   score++;
   scoreBoard.textContent = score;
-  console.log(this);
+  saveScoreLs(score);
   this.parentNode.classList.remove("up");
+}
+
+function saveScoreLs(score) {
+  localStorage.setItem("currentScore", score);
 }
 
 moles.forEach((mole) => mole.addEventListener("click", bonk));
